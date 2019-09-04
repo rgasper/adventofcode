a0ff74957bdcc5405ba7cb12f1ab79b1e9a85e17
Raymond Gasper <rgasper@netprospex.com>
2019-09-04 15:52:46 -0400

    part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,31 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
-    let data = fs::read_to_string("input")
-        .expect("Unable to read file");
-    let mut frequency = 0;
-    for change in data.split("\n") {
-        let num_chg: i32 = match change.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-            };
-        frequency += num_chg;                          
-        println!("num change is {}, frequency is now {}", num_chg, frequency);  
+    let mut frequencies: HashSet<i32> = HashSet::new();
+    let mut frequency: i32 = 0;
+    frequencies.insert(frequency);
+    let mut flag: bool = false; // using a flag is ugly but it does work
+    loop {
+        let data = fs::read_to_string("input")
+            .expect("Unable to read file");
+        for change in data.split("\n") {
+            let num_chg: i32 = match change.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+                };
+            frequency += num_chg;      
+            println!("frequency is now {}", frequency);
+            if frequencies.contains(&frequency) {
+                println!("First frequency encountered twice is {}", frequency);
+                flag = true;
+                break;
+            } else {
+                frequencies.insert(frequency);
+            }
+        }
+        if flag {
+            break;
+        }
     }
 }
