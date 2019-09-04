c086ded5a0b0c0930c57eb961f59571eb962525f
Raymond Gasper <rgasper@netprospex.com>
2019-09-04 15:20:37 -0400

    day 1 part one done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,15 @@
+use std::fs;
 
 fn main() {
-    println!("hi there");
+    let data = fs::read_to_string("input")
+        .expect("Unable to read file");
+    let mut frequency = 0;
+    for change in data.split("\n") {
+        let num_chg: i32 = match change.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+            };
+        frequency += num_chg;                          
+        println!("num change is {}, frequency is now {}", num_chg, frequency);  
+    }
 }
