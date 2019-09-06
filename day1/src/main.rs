e502b1e7701e26bcd1abd5c84a5bcf39d8949206
Raymond Gasper <rgasper@netprospex.com>
2019-09-06 08:40:38 -0400

    it works and it prints debug, cool

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs;
-use std::collections::HashSet;
 
 fn main() {
-    let mut frequencies: HashSet<i32> = HashSet::new();
-    let mut frequency: i32 = 0;
-    frequencies.insert(frequency);
     let data = fs::read_to_string("input")
         .expect("Unable to read file");
-    let data_iter = data.split_whitespace();
-    data_iter.map(|numstr| numstr.trim().parse());
+    let data_iter = data.split("\n");
+    let ff: i32 = data_iter.map(|numstr| numstr.trim().parse::<i32>())
+                           .map(|freq| if let Ok(f) = freq {println!("freq change is {}", f); f} else {0})
+                           .sum();
+    println!("Final Frequency is {}", ff);
 }
