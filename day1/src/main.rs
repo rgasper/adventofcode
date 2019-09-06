19ebcf0242e0c3c1f59dc696243a30896f817828
Raymond Gasper <rgasper@netprospex.com>
2019-09-06 08:02:00 -0400

    working on it

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,27 +5,8 @@ fn main() {
     let mut frequencies: HashSet<i32> = HashSet::new();
     let mut frequency: i32 = 0;
     frequencies.insert(frequency);
-    let mut flag: bool = false; // using a flag is ugly but it does work
-    loop {
-        let data = fs::read_to_string("input")
-            .expect("Unable to read file");
-        for change in data.split("\n") {
-            let num_chg: i32 = match change.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-                };
-            frequency += num_chg;      
-            println!("frequency is now {}", frequency);
-            if frequencies.contains(&frequency) {
-                println!("First frequency encountered twice is {}", frequency);
-                flag = true;
-                break;
-            } else {
-                frequencies.insert(frequency);
-            }
-        }
-        if flag {
-            break;
-        }
-    }
+    let data = fs::read_to_string("input")
+        .expect("Unable to read file");
+    let data_iter = data.split_whitespace();
+    data_iter.map(|numstr| numstr.trim().parse());
 }
