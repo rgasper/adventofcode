5a50af5b04b58af9f4e99ccf4c631f1247876a10
Raymond Gasper <rgasper@netprospex.com>
2019-11-04 10:05:35 -0500

    read contents

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
+
     let args: Vec<String> = env::args().collect();
     let query = &args[1];    
     let filename = &args[2];
-    
-    println!("looking for pattern {} in file {}", query, filename)
+    println!("looking for pattern \"{}\" in file \"{}\"", query, filename);
+
+    let contents = fs::read_to_string(filename)
+                    .expect("cannot read file");
+    println!("contents of file:\n {}", contents)
 }
